ac9db091788c20d870b286a113adaa7420fc7a09
piyushraj15 <piyush.raj73@gmail.com>
2020-04-08 17:40:19 +0530

    setup the api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import styles from './App.module.css';
+
+import Axios from 'axios'
+
+import {Cards ,Chart ,Country} from './components'
+
+import {getData} from './api'
+
+const url=" https://covid19.mathdro.id/api"
 
 class App extends Component {
+  
+  async componentDidMount(){
+
+    const data = await getData();
+
+    console.log(data)
+  }
+  
+  
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards/>
+        <Country/>
+        <Chart/>
       </div>
     )
   }
