10e6ea8071805b928735f35bf54c1c70d362cd37
piyushraj15 <piyush.raj73@gmail.com>
2020-04-14 19:54:51 +0530

    Updates

---
+++ precache-manifest.a58afb9a7af94ede864a27e9e2aedff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76ad5a6011655d003ea6ba9f82fada8",
+    "url": "/Cornavirus-Tracker-App/index.html"
+  },
+  {
+    "revision": "385e76a3a4c5c02100a3",
+    "url": "/Cornavirus-Tracker-App/static/css/main.d690d7a7.chunk.css"
+  },
+  {
+    "revision": "bf6e68578f413c17c61f",
+    "url": "/Cornavirus-Tracker-App/static/js/2.ac590e59.chunk.js"
+  },
+  {
+    "revision": "5acd701488925a686ccd17efe91b80b2",
+    "url": "/Cornavirus-Tracker-App/static/js/2.ac590e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385e76a3a4c5c02100a3",
+    "url": "/Cornavirus-Tracker-App/static/js/main.769bf01d.chunk.js"
+  },
+  {
+    "revision": "fbf8903808b38b73f0f1",
+    "url": "/Cornavirus-Tracker-App/static/js/runtime-main.533292bf.js"
+  },
+  {
+    "revision": "b261db91491c12d28d73e22a440819af",
+    "url": "/Cornavirus-Tracker-App/static/media/imagecovid.b261db91.jpg"
+  }
+]);
